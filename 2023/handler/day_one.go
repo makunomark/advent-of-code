0a85158a4fbc98cf83ee3fa39252310fb71ef0d7
makunomark <makunomark@gmail.com>
2023-12-25 11:38:16 +0300

    Got sample data sum

--- 2023/handler/day_one.go
+++ 2023/handler/day_one.go
@@ -1,6 +1,10 @@
 package handler
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
 
 func DayOne() {
 	data, err := ReadFile("day_one.mock.txt")
@@ -8,7 +12,41 @@ func DayOne() {
 		fmt.Println(err)
 		return
 	}
+
+	calibrationValueSum := 0
 	for _, line := range data {
-		fmt.Println(line)
+		// for each line, get first digit and last digit
+		// we can do this by converting the string to a slice of runes
+		lineRunes := []rune(line)
+		// lets find the first digit
+		firstDigit := 0
+		for _, r := range lineRunes {
+			if unicode.IsDigit(r) {
+				firstDigit = convertRuneToInt(r)
+				break
+			}
+		}
+
+		lastDigit := 0
+		for i := len(lineRunes) - 1; i >= 0; i-- {
+			if unicode.IsDigit(lineRunes[i]) {
+				lastDigit = convertRuneToInt(lineRunes[i])
+				break
+			}
+		}
+
+		calibrationValue := (firstDigit * 10) + lastDigit
+		calibrationValueSum += calibrationValue
 	}
+
+	fmt.Println("Calibration value sum:", calibrationValueSum)
+}
+
+func convertRuneToInt(r rune) int {
+	digit, err := strconv.Atoi(string(r))
+	if err != nil {
+		fmt.Println("Error converting rune to digit:", err)
+		return 0
+	}
+	return digit
 }
